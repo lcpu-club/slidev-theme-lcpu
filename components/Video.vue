9ccec720cfa078cf53e75f2fec727009087e0c88
HolgerHuo <50446405+HolgerHuo@users.noreply.github.com>
2025-10-21 23:40:25 +0800

    add: Video component

---
+++ components/Video.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="rounded overflow-hidden h-95 w-fit mx-auto" style="margin-top: -25px">
+    <video ref="videoElement" class="w-full h-full" muted />
+  </div>
+  <div v-if="caption" class="text-center mt-4">
+    {{ caption }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { onSlideEnter } from '@slidev/client'
+
+const props = defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  caption: {
+    type: String,
+    default: ''
+  }
+})
+
+const videoElement = ref(null);
+
+onSlideEnter(() => {
+  if (videoElement.value) {
+    videoElement.value.src = props.src
+    videoElement.value.currentTime = 0
+    videoElement.value.play()
+  }
+})
+</script>
