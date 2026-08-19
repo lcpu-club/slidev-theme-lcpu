e0567b536f4c0bc852a4e615cbfcd809c8eaa454
HolgerHuo <50446405+HolgerHuo@users.noreply.github.com>
2026-08-19 22:51:20 +0800

    Add Image component with props for src and caption

---
+++ components/Image.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="rounded overflow-hidden h-95 w-fit mx-auto" style="margin-top: -25px">
+    <img class="w-full h-full" :src="src" />
+  </div>
+  <div v-if="caption" class="text-center mt-4">
+    {{ caption }}
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  caption: {
+    type: String,
+    default: ''
+  }
+})
+</script>
