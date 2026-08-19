bdaac400587da6525da1930da6ac40784ea1ac01
HolgerHuo <50446405+HolgerHuo@users.noreply.github.com>
2026-08-19 23:35:01 +0800

    Add center layout with CoverCornerBadge component

---
+++ layouts/center.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import CoverCornerBadge from "../components/CoverCornerBadge.vue";
+</script>
+
+<template>
+  <div class="slidev-layout center h-full grid place-content-center">
+    <div class="my-auto">
+      <slot />
+    </div>
+    <CoverCornerBadge />
+  </div>
+</template>
